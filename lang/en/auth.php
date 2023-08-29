d88e947ce099812d19ca0e22f395a26c45be230f
Raxmatilla <56291918+Raxmatilla97@users.noreply.github.com>
2023-08-29 11:15:50 +0500

    Zaibis ko'p narsa bo'ldi.

---
+++ lang/en/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => "Bu email ma'lumoti to'g'ri emas yoki ro'yxatda hali mavjud emas.",
+    'password' => 'Berilgan parol noto‘g‘ri.',
+    'throttle' => "Kirish uchun juda koʻp urinishlar. Iltimos, :seconds soniyadan keyin qayta urinib ko'ring.",
+
+];
